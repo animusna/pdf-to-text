c4450a7a050f38bd902154c95f81922efd64ba08
animusna <mattocfb@gmail.com>
2023-03-15 11:07:07 +0100

    Beta version

---
+++ README.md
@@ -0,0 +1,52 @@
+# pdf-to-text
+**pdf-to-text** is an utility to extract text content from PDF files using [OCR](https://en.wikipedia.org/wiki/Optical_character_recognition) (**Optical Character Recognition**).
+This is utility use the package [**Tesseract OCR**](https://github.com/tesseract-ocr/tesseract) through the python library [**pytesseract**](https://pypi.org/project/pytesseract/). Also the utility uses the python library [**pdf2image**](https://pypi.org/project/pdf2image/) to extract images from pdf documents.
+
+## Requirements
+On your workstation you must have installed:
+- Python 3.10
+- TesserAct (follow the [guide](https://tesseract-ocr.github.io/tessdoc/Installation.html))
+- TesserAct language pack (in case you decide to use a specific language)
+
+## How the utility works
+
+The utility elaborate all pdf files contained in the **input directory** (by default is **./data/in**) and produce the text content in **the output folder** (by default is **./data/out**).
+
+### Examples
+
+If you have in the input folders the following files:
+
+```
+data
+|
+|-in
+|  |-mydoc-01.pdf
+|  |-mydoc-02.pdf
+|
+```
+
+The utility will produce the text content in the output folder like shown below.
+
+```
+data
+|
+|-in
+|  |-mydoc-01.pdf
+|  |-mydoc-02.pdf
+|
+|-out
+|  |
+|  |-mydoc-01
+|  |    |
+|  |    |-mydoc-01_0.pdf
+|  |    |-mydoc-01_1.pdf
+|  |    |-mydoc-01.txt
+|  |    
+|  |-mydoc-02
+|  |    |
+|  |    |-mydoc-02_0.pdf
+|  |    |-mydoc-02_1.pdf
+|  |    |-mydoc-02.txt
+```
+
+The utility for each PDF file will produce a text file and *n* image file where *n* is the number of the pages of the document.
---
+++ main.py
@@ -0,0 +1,63 @@
+import os
+from PIL import Image
+from pdf2image import convert_from_path
+import pytesseract
+
+input_dir = os.path.join('.', "data", "in")
+output_dir = os.path.join('.', "data", "out")
+language = None
+
+def convertPdfToImages(file_path: str, file_name: str, output_file_dir: str):
+    print("\tGenerating temporary images...")
+    images = convert_from_path(file_path)
+    page_prefix = file_name
+
+    for i in range(len(images)):
+        image_file_name = page_prefix + "_" + str(i) + '.jpg'
+        print(f"\t\tGenerating temporary images {image_file_name}...")
+        images[i].save(os.path.join(output_file_dir, image_file_name), 'JPEG')
+
+
+def get_text(output_file_dir: str, file_name: str):
+    images = os.listdir(output_file_dir)
+    if not images:
+        print("\t\tNo images found")
+        exit
+    ocr_file = os.path.join(output_file_dir, f"{file_name}.txt")
+    with open(ocr_file, 'w') as f:
+        f.write('')
+
+    for image in images:
+        if (image.endswith("jpg")):
+            print(f"\t\t OCR processing on {image}...")
+            image_text = pytesseract.image_to_string(
+                Image.open(os.path.join(output_file_dir, image)), lang = language)
+            with open(ocr_file, 'a') as f:
+                f.write(image_text)
+
+if __name__ == "__main__":
+    print("\n********************************")
+    print("* Welcome to PDF to TEXT Utility*")
+    print("*********************************\n")
+
+    print("Seeking pdf in input folder...\n")
+    files = os.listdir(input_dir)
+
+    if not files:
+        print("No files on input folder\n")
+        exit
+
+    for file in files:
+        if (file.endswith("pdf")):
+            print(f"\nElaborating {file}...")
+            file_name = file.split(".pdf")[0]
+            file_temp_dir = os.path.join(output_dir, file_name)
+
+            if not os.path.isdir(file_temp_dir):
+                print(f"\tMaking temporarty directory {file_temp_dir}...")
+                os.mkdir(file_temp_dir)
+
+            convertPdfToImages(os.path.join(input_dir, file), file_name, file_temp_dir)
+
+            print(f"\tStarting OCR on pdf pages...")
+            get_text(file_temp_dir, file_name)
